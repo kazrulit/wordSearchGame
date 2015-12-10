632e879ed742ab29e48876c588785a5664713608
kazrulit <mr_spec@mail.ru>
2015-12-10 15:33:00 +0600

    Added O and E lettern pattern

---
+++ src/main/java/kz/epam/model/pattern/ELetterPattern.java
@@ -0,0 +1,64 @@
+package kz.epam.model.pattern;
+
+/**
+ * Created by Khamid_Sarmanov on 12/10/2015.
+ */
+public class ELetterPattern implements LetterPattern {
+    public char[][] fillLeter(String... word) {
+        char[][] grid = new char[CharHeight + (GridHeight - CharHeight)][CharWidth + (GridWidth - CharWidth)];
+
+        int yDifference = (GridHeight - CharHeight)/2;
+        int xDifference = (GridWidth - CharWidth)/2;
+
+        String top = word[0].trim();
+        String left = word[1].trim();
+        String bottom = word[2].trim();
+        String middle = word[3].trim();
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+
+                /** TOP horizontal **/
+                if(i == yDifference) {
+                    if(top.length() > (j -xDifference) && xDifference < j + 1 )
+                        grid[i][j] = top.charAt(j - xDifference);
+                    else
+                        grid[i][j] = '-';
+                }
+
+                /** Left vertical **/
+                else if(j == xDifference) {
+                    if(left.length() > (i - yDifference) && yDifference < i + 1)
+                        grid[i][j] = left.charAt(i - yDifference);
+                    else
+                        grid[i][j] = '-';
+                }
+
+
+                /** Middle **/
+                else if(i == yDifference + 3) {
+                    if(middle.length() > (j - xDifference) && xDifference < j + 1 )
+                        grid[i][j] = middle.charAt(j - xDifference);
+                    else
+                        grid[i][j] = '-';
+                }
+
+                /** Bottom **/
+                else if(i == grid.length - yDifference - 1) {
+                    if(bottom.length() > (j - xDifference) && xDifference < j + 1 )
+                        grid[i][j] = bottom.charAt(j - xDifference);
+                    else
+                        grid[i][j] = '-';
+                }
+
+
+
+                else {
+                    grid[i][j] = '-';
+                }
+            }
+        }
+
+        return grid;
+    }
+}
---
+++ src/test/java/kz/epam/model/pattern/OLetterPatternTest.java
@@ -0,0 +1,22 @@
+package kz.epam.model.pattern;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+/**
+ * Created by Khamid_Sarmanov on 12/10/2015.
+ */
+public class OLetterPatternTest extends TestCase {
+    @Test
+    public void testFillLeter() throws Exception {
+        LetterPattern letterPattern = new OLetterPattern();
+        char[][] grid = letterPattern.fillLeter("board", "beginner", "decadent", "roast");
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                System.out.print(grid[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
